9d7fd2c7fade031c941d0e62e66735ce9f5a2194
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-18 19:53:48 -0300

    feat: create Tweet model

---
+++ src/main/java/com/tweteroospring/model/Tweet.java
@@ -0,0 +1,28 @@
+package com.tweteroospring.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tweet {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column
+  private String username;
+
+  @Column
+  private String avatar;
+
+  @Column
+  private String text;
+}
