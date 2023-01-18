68b2da5e699943f72caafef6a197e541e15e6f13
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-18 19:32:52 -0300

    feat: Create project with spring initializr

---
+++ src/main/java/com/tweteroospring/TweterooSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tweteroospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweterooSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweterooSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweteroospring/TweterooSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
