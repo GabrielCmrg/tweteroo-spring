9947bf37a3abb33ea0cca64a03fb7e0722cb7982
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-18 23:04:35 -0300

    feat: add GET to /tweets that return a list of tweets

--- src/main/java/com/tweteroospring/controller/TweetController.java
+++ src/main/java/com/tweteroospring/controller/TweetController.java
@@ -1,6 +1,9 @@
 package com.tweteroospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class TweetController {
   public String postTweet(@RequestBody TweetDTO reqTweetDTO) {
     repository.save(new Tweet(reqTweetDTO));
     return "OK";
+  }
+
+  @GetMapping
+  public List<Tweet> findAll() {
+    return repository.findAll();
   }
 }
