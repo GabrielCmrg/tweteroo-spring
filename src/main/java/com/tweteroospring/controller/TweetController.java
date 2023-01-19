126ee24c902b4f854926b27f59e82abce8ec7037
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-18 23:01:20 -0300

    feat: add POST to /tweets

--- src/main/java/com/tweteroospring/controller/TweetController.java
+++ src/main/java/com/tweteroospring/controller/TweetController.java
@@ -1,9 +1,13 @@
 package com.tweteroospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroospring.dto.TweetDTO;
+import com.tweteroospring.model.Tweet;
 import com.tweteroospring.repository.TweetRepository;
 
 @RestController
@@ -12,4 +16,10 @@ public class TweetController {
 
   @Autowired
   private TweetRepository repository;
+
+  @PostMapping
+  public String postTweet(@RequestBody TweetDTO reqTweetDTO) {
+    repository.save(new Tweet(reqTweetDTO));
+    return "OK";
+  }
 }
