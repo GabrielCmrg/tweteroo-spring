8eb0ba828343b76e9058997474a7b4ec775d4cb6
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-24 17:01:51 -0300

    chore: add table names to entities

--- src/main/java/com/tweteroospring/model/Tweet.java
+++ src/main/java/com/tweteroospring/model/Tweet.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "TWEETS")
 @NoArgsConstructor
 public class Tweet {
 
