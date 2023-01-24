a0f8d12a5176acfe0c2ba5597ce1d750029e74cc
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-24 18:21:58 -0300

    feat: add pagination to get tweets

--- src/main/java/com/tweteroospring/controller/TweetController.java
+++ src/main/java/com/tweteroospring/controller/TweetController.java
@@ -1,12 +1,12 @@
 package com.tweteroospring.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroospring.dto.TweetReqDTO;
@@ -27,7 +27,7 @@ public class TweetController {
   }
 
   @GetMapping
-  public List<Tweet> findAll() {
-    return service.findAll();
+  public Page<Tweet> findAll(@RequestParam int page) {
+    return service.findAll(page);
   }
 }
--- src/main/java/com/tweteroospring/service/TweetService.java
+++ src/main/java/com/tweteroospring/service/TweetService.java
@@ -1,8 +1,10 @@
 package com.tweteroospring.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.tweteroospring.dto.TweetReqDTO;
@@ -27,7 +29,9 @@ public class TweetService {
     tweetRepository.save(new Tweet(tweetSaveDTO));
   }
 
-  public List<Tweet> findAll() {
-    return tweetRepository.findAll();
+  public Page<Tweet> findAll(int page) {
+    final int ITEMS_PER_PAGE = 5;
+    Pageable pageSpecs = PageRequest.of(page, ITEMS_PER_PAGE, Sort.by("id").descending());
+    return tweetRepository.findAll(pageSpecs);
   }
 }
